b6f31f5865124f0fc0b145eb26a4001929713270
Imaginary-jumper <ankit2102karmakar@gmail.com>
2025-06-05 03:12:05 +0530

    Add files via upload

---
+++ trial1.py
@@ -0,0 +1,7 @@
+list1= []
+list_extract= []
+for i in range(1,11):
+    list1.append(i)
+print("original list:",list1)
+print("extracted first five elements:",list1[0:5])
+print("reversed ectracted elements:",list1[4::-1])
