0c9aa7061c3cc37063b44cf6a71d3b5f474b824f
Imaginary-jumper <ankit2102karmakar@gmail.com>
2025-06-05 03:21:09 +0530

    Update trial1.py

--- trial1.py
+++ trial1.py
@@ -4,4 +4,5 @@ for i in range(1,11):
     list1.append(i)
 print("original list:",list1)
 print("extracted first five elements:",list1[0:5])
-print("reversed ectracted elements:",list1[4::-1])
+list1.reverse()
+print("reversed extracted elements:",list1[5:])
